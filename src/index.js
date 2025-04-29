4bf316120ce1a6b22ae71ee5e04b9284d31c6732
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-04-29 11:52:04 -0400

    Add function to take in inputs add function to fetch weather data based on input

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-console.log("hi");
-
 //key XQCVRZLS6VS9ZR2P644NHNLQH
 
 //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=11111111111111111
 
-// fetch(
-//   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/columbus?unitGroup=us&include=days&key=XQCVRZLS6VS9ZR2P644NHNLQH&contentType=json",
-//   { mode: "cors" }
-// )
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (response) {
-//     console.log(response.days);
-//     console.log(response.days[0].datetime);
-//     console.log(response.days[0].conditions);
-//     console.log(response.days[0].feelslike);
-//     console.log(response.days[0].temp);
-//     console.log(response.days[0].tempmax);
-//     console.log(response.days[0].tempmin);
-//     console.log(response.days[0].description);
-//   });
+const input = function () {
+  const city = document.querySelector(".city");
+  const date = document.querySelectorAll(".day");
+  for (let x = 0; x < date.length; x++) {
+    if (date[x].checked) {
+      datecheck = date[x].id;
+      date[x].checked = false;
+    }
+  }
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city.value}?unitGroup=us&include=days&key=XQCVRZLS6VS9ZR2P644NHNLQH&contentType=json`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      if (datecheck == "today") {
+        console.log(response.days[0].datetime);
+        console.log(response.days[0].conditions);
+        console.log(response.days[0].feelslike);
+        console.log(response.days[0].temp);
+        console.log(response.days[0].tempmax);
+        console.log(response.days[0].tempmin);
+        console.log(response.days[0].description);
+      } else if (datecheck == "tomorrow") {
+        console.log(response.days[1].datetime);
+        console.log(response.days[1].conditions);
+        console.log(response.days[1].feelslike);
+        console.log(response.days[1].temp);
+        console.log(response.days[1].tempmax);
+        console.log(response.days[1].tempmin);
+        console.log(response.days[1].description);
+      }
+    });
+  city.value = "";
+};
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", input);
