b9db3946465259d8b4d6ceb1197e686aba2b18d5
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-04-29 11:22:43 -0400

    Add input and buttons to html add fetch for weather data

---
+++ src/index.js
@@ -0,0 +1,23 @@
+console.log("hi");
+
+//key XQCVRZLS6VS9ZR2P644NHNLQH
+
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=11111111111111111
+
+// fetch(
+//   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/columbus?unitGroup=us&include=days&key=XQCVRZLS6VS9ZR2P644NHNLQH&contentType=json",
+//   { mode: "cors" }
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (response) {
+//     console.log(response.days);
+//     console.log(response.days[0].datetime);
+//     console.log(response.days[0].conditions);
+//     console.log(response.days[0].feelslike);
+//     console.log(response.days[0].temp);
+//     console.log(response.days[0].tempmax);
+//     console.log(response.days[0].tempmin);
+//     console.log(response.days[0].description);
+//   });
