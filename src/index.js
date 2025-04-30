16a412249ab1daf39205b5651b277c5ce8c9b3c4
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-04-30 12:01:20 -0400

    Add style to page modify function to display 7 days of weather remove today tomorrow selection

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
+import "./styles.css";
+
 const input = function () {
   const city = document.querySelector(".city");
-  const date = document.querySelectorAll(".day");
-  const outputdate = document.querySelector(".date");
-  const outputconditions = document.querySelector(".conditions");
-  const outputfeels = document.querySelector(".feelslike");
-  const outputtemp = document.querySelector(".temp");
-  const outputtempmax = document.querySelector(".tempmax");
-  const outputtempmin = document.querySelector(".tempmin");
-  const outputdescription = document.querySelector(".description");
+  const container = document.querySelector(".container");
 
-  for (let x = 0; x < date.length; x++) {
-    if (date[x].checked) {
-      datecheck = date[x].id;
-      date[x].checked = false;
-    }
-  }
   fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city.value}?unitGroup=us&include=days&key=XQCVRZLS6VS9ZR2P644NHNLQH&contentType=json`,
     { mode: "cors" }
@@ -23,22 +12,47 @@ const input = function () {
       return response.json();
     })
     .then(function (response) {
-      if (datecheck == "today") {
-        outputdate.textContent = `Weather for ${response.days[0].datetime}.`;
-        outputconditions.textContent = `Conditions for the day are ${response.days[0].conditions}.`;
-        outputfeels.textContent = `The temperature feels like ${response.days[0].feelslike}.`;
-        outputtemp.textContent = `The temperature for the day is ${response.days[0].temp}.`;
-        outputtempmax.textContent = `The max temperature for the day is ${response.days[0].tempmax}.`;
-        outputtempmin.textContent = `The min temperature for the day is ${response.days[0].tempmin}.`;
-        outputdescription.textContent = response.days[0].description;
-      } else if (datecheck == "tomorrow") {
-        outputdate.textContent = `Weather for ${response.days[1].datetime}.`;
-        outputconditions.textContent = `Conditions for the day are ${response.days[1].conditions}.`;
-        outputfeels.textContent = `The temperature feels like ${response.days[1].feelslike}.`;
-        outputtemp.textContent = `The temperature for the day is ${response.days[1].temp}.`;
-        outputtempmax.textContent = `The max temperature for the day is ${response.days[1].tempmax}.`;
-        outputtempmin.textContent = `The min temperature for the day is ${response.days[1].tempmin}.`;
-        outputdescription.textContent = response.days[1].description;
+      for (let x = 0; x < 7; x++) {
+        console.log(response.days[x]);
+
+        const day = document.createElement("div");
+        day.classList.add("day");
+        container.appendChild(day);
+
+        const outputdate = document.createElement("div");
+        outputdate.classList.add("date");
+        outputdate.textContent = `Weather for ${response.days[x].datetime}.`;
+        day.appendChild(outputdate);
+
+        const outputconditions = document.createElement("div");
+        outputconditions.classList.add("conditions");
+        outputconditions.textContent = `Conditions for the day are ${response.days[x].conditions}.`;
+        day.appendChild(outputconditions);
+
+        const outputfeels = document.createElement("div");
+        outputfeels.classList.add("feelslike");
+        outputfeels.textContent = `Conditions for the day are ${response.days[x].feelslike}.`;
+        day.appendChild(outputfeels);
+
+        const outputtemp = document.createElement("div");
+        outputtemp.classList.add(".temp");
+        outputtemp.textContent = `The temperature for the day is ${response.days[x].temp}.`;
+        day.appendChild(outputtemp);
+
+        const outputtempmax = document.createElement("div");
+        outputtempmax.classList.add(".tempmax");
+        outputtempmax.textContent = `The max temperature for the day is ${response.days[x].tempmax}.`;
+        day.appendChild(outputtempmax);
+
+        const outputtempmin = document.createElement("div");
+        outputtempmin.classList.add(".tempmin");
+        outputtempmin.textContent = `The min temperature for the day is ${response.days[x].tempmin}.`;
+        day.appendChild(outputtempmin);
+
+        const outputdescription = document.createElement("div");
+        outputdescription.classList.add(".description");
+        outputdescription.textContent = response.days[x].description;
+        day.appendChild(outputdescription);
       }
     });
   city.value = "";
